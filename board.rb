ffa96041605dea358314118038fcf5ab68ca8a54
Jake Burton <jburto94@yahoo.com>
2021-08-20 13:44:45 -0400

    Create basic tile class and board render method

--- board.rb
+++ board.rb
@@ -3,17 +3,26 @@ require_relative "tile"
 class Board
   def self.empty_grid
     Array.new(9) do
-      Array.new(9) { Tile.new(0) }
+      Array.new(9) { Tile.new("E") }
     end
   end
 
-  def initialize(grid = self.empty_grid)
+  def initialize(grid = self.class.empty_grid)
     @grid = grid
   end
 
   def [](pos)
     x,y = pos
     grid[x][y]
+  end
+
+  def render
+    puts "  #{(0..8).to_a.join(" ")}"
+    grid.each_with_index do |row, index|
+      print "#{index}"
+      row.each { |tile| print " #{tile.value}"}
+      print "\n"
+    end
   end
 
   private
--- tile.rb
+++ tile.rb
@@ -1,3 +1,19 @@
-class Tile
+require "colorize"
 
+class Tile
+  attr_reader :value
+  attr_writer :turned
+
+  def initialize(value)
+    @value = value
+    @turned = false
+  end
+
+  def color
+    value == "B" ? :red : :green
+  end
+
+  def to_s
+    @turned ? value : " "
+  end
 end
